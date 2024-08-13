fa0c2f95a5d38c8d2a1eecaec23fd99509513a00
andreashl09 <a9279352@gmail.com>
2024-08-13 23:55:11 +0200

    game mechanics implemented

--- src/main/java/de/example/Main.java
+++ src/main/java/de/example/Main.java
@@ -1,8 +1,147 @@
 package de.example;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static PlayingField game;
+
     public static void main(String[] args) {
-        PlayingField game = new PlayingField(10,10);
-        game.printArray();
+        displayStart();
+        boolean isUserInput = false;
+        Scanner scanner = new Scanner(System.in);
+
+        while (!isUserInput) {
+            System.out.println("Anfänger = 1, Fortgeschritten = 2, Profi = 3 | Anleitung = 9");
+            System.out.println("Deine Auswahl:");
+            int userChoice = convertingStartInput(scanner.next());
+            if (userChoice == -1) {
+                return;
+            } else if (userChoice == 9) {
+                System.out.println("Anleitung");
+            } else if (userChoice > 0 && userChoice <= 3) {
+                game = setPlayingField(userChoice);
+                isUserInput = true;
+            } else {
+                System.out.println("Falsche Eingabe!");
+            }
+        }
+
+        Scanner scanner1 = new Scanner(System.in);
+        int gameRun = 0;
+        while (true) {
+            clearScreen();
+            game.printArray();
+            System.out.println("Mine:" + game.getMines());
+            System.out.println("Feld aufdecken = (r Reihe Spalte) und Markieren = (f Reihe Spalte)");
+            System.out.println("Gib deinen Zug ein:");
+            String userInput = scanner1.nextLine();
+            if (userInput.equals("EXIT")) return;
+            gameRun = convertingWhileRun(userInput);
+            if (gameRun == -1) {
+                game.clearThePlayingField();
+                game.printArray();
+                System.out.println("Leider verloren!");
+                return;
+            }
+
+        }
+//scanner1.close();
+//        scanner.close();
+    }
+
+    public static int convertingStartInput(String userInput) {
+        if (userInput.equals("EXIT")) return -1;
+        if (userInput.length() == 1 && Character.isDigit(userInput.charAt(0)))
+            return Integer.parseInt(String.valueOf(userInput.charAt(0)));
+        return 0;
+    }
+
+    public static int convertingWhileRun(String userInput) {
+        if (userInput.length() >= 5 && userInput.charAt(1) == ' ' &&
+                (userInput.startsWith("f ") || userInput.startsWith("r "))) {
+
+            String[] input = userInput.split(" ");
+            String letter = input[0];
+            int row = Integer.parseInt(input[1]);
+            int column = Integer.parseInt(input[2]);
+
+            if (row < game.getRow() && column < game.getColumn()) {
+                return setUncoverField(letter, row, column);
+            }
+        } else {
+            System.out.println("Falsche Eingabe!");
+        }
+        return 0;
+    }
+
+    public static int setUncoverField(String letter, int row, int column) {
+        char[][] gameFieldUser = game.getFieldUser();
+        char[][] gameField = game.getField();
+
+        if (letter.equals("f")) {
+            gameFieldUser[row][column] = 'F';
+            game.setMines(game.getMines() - 1);
+            return 0;
+        }
+
+        if (gameFieldUser[row][column] != '.') return 0;
+        if (gameField[row][column] == '*') return -1; // Game End
+
+        game.clearEmptyFields(row, column); //
+
+        if (letter.equals("r")) {
+            gameFieldUser[row][column] = gameField[row][column];
+        }
+        return 0;
+    }
+
+
+    public static PlayingField setPlayingField(int userChoice) {
+        int rows = 0, column = 0, mine = 0;
+        switch (userChoice) {
+            case 1:
+                rows = 10;
+                column = 10;
+                mine = 10;
+                break;
+            case 2:
+                rows = 17;
+                column = 17;
+                mine = 40;
+                break;
+            case 3:
+                rows = 17;
+                column = 31;
+                mine = 99;
+                break;
+            default:
+                System.out.println("Falsche Eingabe");
+                break;
+        }
+        return new PlayingField(rows, column, mine);
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void displayStart() {
+        String[] minesweeperArt = {
+                "███    ███ ██ ███    ██ ███████ ███████ ██     ██ ███████ ███████ ██████  ███████ ██████  ",
+                "████  ████ ██ ████   ██ ██      ██      ██     ██ ██      ██      ██   ██ ██      ██   ██ ",
+                "██ ████ ██ ██ ██ ██  ██ █████   ███████ ██  █  ██ █████   █████   ██████  █████   ██████  ",
+                "██  ██  ██ ██ ██  ██ ██ ██           ██ ██ ███ ██ ██      ██      ██      ██      ██   ██ ",
+                "██      ██ ██ ██   ████ ███████ ███████  ███ ███  ███████ ███████ ██      ███████ ██   ██ "
+
+        };
+        clearScreen();
+        for (String line : minesweeperArt) {
+            System.out.println(line);
+        }
+        System.out.println();
+        System.out.println("Wähle dein Schwierigkeitsgrad:");
+        System.out.println("Mit der Eingabe von EXIT kannst du jederzeit das Spiel verlassen.");
     }
 }
--- src/main/java/de/example/PlayingField.java
+++ src/main/java/de/example/PlayingField.java
@@ -1,5 +1,8 @@
 package de.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayingField {
 
     private final int column;
@@ -10,16 +13,41 @@ public class PlayingField {
     private final char HIDDENFIELD = '.';
     private final char BUOY = 'F';
     private final char EMPTY_FIELD = '0';
+    private int mines;
 
-
-    public PlayingField(int column, int width) {
+    public PlayingField(int rows, int column, int mine) {
         this.column = column;
-        this.row = width;
-        int mines = numberOfMines(column, width);
+        this.row = rows;
+        this.mines = mine;
         this.field = charsArray(EMPTY_FIELD);
         this.fieldUser = charsArray(HIDDENFIELD);
-        setMines(mines);
+        mineInField(mine);
     }
+
+    public int getMines() {
+        return mines;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public char[][] getField() {
+        return field;
+    }
+
+    public char[][] getFieldUser() {
+        return fieldUser;
+    }
+
+    public void setMines(int mines) {
+        this.mines = mines;
+    }
+    //
 
     private int numberOfMines(int length, int width) {
         return length * width / 5;
@@ -35,36 +63,53 @@ public class PlayingField {
         return arr;
     }
 
-    private int setMines(int mines) {
+    private int mineInField(int mines) {
         while (mines > 0) {
             int randomColumn = (int) (Math.random() * this.column);
             int randomRow = (int) (Math.random() * this.row);
-            if (this.field[randomRow][randomColumn] == MINE) return setMines(mines);
+            if (this.field[randomRow][randomColumn] == MINE) return mineInField(mines);
             this.field[randomRow][randomColumn] = MINE;
-            determineTheNumbers(randomRow,randomColumn);
+            determineTheNumbers(randomRow, randomColumn);
             --mines;
         }
         return 0;
     }
 
     public void printArray() {
+        System.out.print("   ");
+        for (int j = 0; j < column; j++) {
+            if (j >= 10) {
+                System.out.print(j / 10 + " ");
+            } else {
+                System.out.print("  ");
+            }
+        }
+        System.out.println();
+        System.out.print("   ");
+        for (int j = 0; j < column; j++) {
+            System.out.print(j % 10 + " ");
+        }
+        System.out.println();
         for (int i = 0; i < row; i++) {
+            System.out.print(i + " ");
+            if (i < 10) System.out.print(" ");
             for (int j = 0; j < column; j++) {
-                System.out.print(field[i][j] + " ");
+                System.out.print(fieldUser[i][j] + " ");
             }
             System.out.println();
         }
     }
 
+
     private void determineTheNumbers(int mineRow, int mineColumn) {
         int startRow = Math.max(0, mineRow - 1);
-        int startColumn = Math.max(0, mineColumn- 1);
-        int endRow = Math.min(field.length, mineRow+2);
-        int endColumn = Math.min(field.length, mineColumn+2);
+        int startColumn = Math.max(0, mineColumn - 1);
+        int endRow = Math.min(field.length, mineRow + 2);
+        int endColumn = Math.min(field[0].length, mineColumn + 2);
 
         while (startRow < endRow) {
 
-            for( int j = startColumn;j < endColumn; j++) {
+            for (int j = startColumn; j < endColumn; j++) {
                 char temp = field[startRow][j];
                 if (temp != MINE) {
                     int number = temp - '0' + 1;
@@ -75,4 +120,62 @@ public class PlayingField {
             ++startRow;
         }
     }
+
+    public void clearEmptyFields(int row, int column) {
+        if (field[row][column] != EMPTY_FIELD) return;
+        List<int[][]> emptyFields = new ArrayList<>();
+
+        int[][] startField = {{row, column}};
+        emptyFields.add(startField);
+
+        while (!emptyFields.isEmpty()) {
+            int[][] tempArr = emptyFields.getFirst();
+            row = tempArr[0][0];
+            column = tempArr[0][1];
+            int startRow = Math.max(0, row - 1);
+            int startColumn = Math.max(0, column - 1);
+            int endRow = Math.min(field.length, row + 2);
+            int endColumn = Math.min(field[0].length, column + 2);
+
+            while (startRow < endRow) {
+
+                for (int j = startColumn; j < endColumn; j++) {
+                    char temp = field[startRow][j];
+                    char tempUser = fieldUser[startRow][j];
+                    if (temp == EMPTY_FIELD && tempUser != EMPTY_FIELD) {
+                        fieldUser[startRow][j] = EMPTY_FIELD;
+                        int[][] emptyField = {{startRow, j}};
+                        emptyFields.add(emptyField);
+                    } else {
+                        fieldUser[startRow][j] = field[startRow][j];
+                    }
+                }
+                ++startRow;
+            }
+            emptyFields.removeFirst();
+        }
+    }
+
+    public void clearThePlayingField() {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if (fieldUser[i][j] == HIDDENFIELD) {
+                    fieldUser[i][j] = field[i][j];
+                }
+                if (fieldUser[i][j] == BUOY && field[i][j] != MINE)
+                    fieldUser[i][j] = '#';
+            }
+        }
+    }
+
+    public int checkWin() {
+        for (int i = 0; i < fieldUser.length; i++) {
+            for (int j = 0; j < fieldUser[0].length; j++) {
+                if (fieldUser[i][j] == HIDDENFIELD) ;
+                return 0;
+            }
+        }
+        return -1;
+    }
+
 }
